d168bea5570c5e90bd28bde07db2c29887cda85c
hemanthoo7 <hemanth.p@jivainfotech.com>
2021-09-30 14:26:05 +0530

    Fifth Commit

---
+++ app/code/Webkul/Hello/Plugin/Checkout/Controller/RestrictProductQuantity.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace [Webkul]\[Hello]\Plugin\Checkout\Controller;
+
+    class RestrictProductQuantity
+    {
+        public function aroundExecute(
+            Index $subject,
+            \Closure $proceed
+        )
+        {
+            //You required logic
+        }
+    }
